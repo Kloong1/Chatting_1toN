9ca6de06e76fb12096a8c703c36d78bcff27a460
Kloong1 <dkdlsmsm98@gmail.com>
2020-01-31 21:36:37 +0900

    ChattingMessage.dll과 그 코드
    Server와 Client가 서로 주고 받을 Message의 정의(ChattingMessage.cs, Body.cs, Header.cs) 그 Message를 송수신하는 데 쓰이는 메소드를 정의한 ChattingMessageUtil.cs

---
+++ ChattingMessage.cs
@@ -0,0 +1,26 @@
+﻿namespace ChattingMessage {
+    public class CONSTANT {
+        public const byte CHATTING = 0x01;
+        public const byte CHANGE_NICKNAME = 0x02;
+        public const byte NOTICE = 0x03;
+        public const byte FLOODING = 0x04;
+    }
+
+    public class Message { //Header & Body
+        
+        public Header header { get; set; }
+        public Body body { get; set; }
+
+        public byte[] GetBytes() {
+            byte[] bytes = new byte[GetSize()];
+            header.GetBytes().CopyTo(bytes, 0);
+            body.GetBytes().CopyTo(bytes, header.GetSize());
+
+            return bytes;
+        }
+
+        public int GetSize() {
+            return header.GetSize() + body.GetSize();
+        }
+    }
+}

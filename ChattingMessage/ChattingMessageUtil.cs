db4dfb6029370f74ce2c0e638067602633bec36e
Kloong1 <dkdlsmsm98@gmail.com>
2020-01-31 21:52:06 +0900

    Add files via upload

---
+++ ChattingMessage/ChattingMessageUtil.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace ChattingMessage {
+    public class ChattingMessageUtil {
+        public static void Send(Stream writer, Message msg) {
+            byte[] bytes = msg.GetBytes();
+            writer.Write(bytes, 0, bytes.Length);
+        }
+
+        public static Message Recieve(Stream reader) {
+            //Read Header
+            int sizeToRead = 5; //sizeof(byte) + sizeof(int)
+            byte[] hBytes = new byte[sizeToRead]; 
+
+            if (reader.Read(hBytes, 0, sizeToRead) == 0) //fail
+                return null;
+
+            Header header = new Header();
+            header.MSGTYPE = hBytes[0];
+
+            //MSGTYPE 값이 예상하지 못한 값일 경우 -> 메세지에 문제가 있음
+            if (header.MSGTYPE < 0x01 || header.MSGTYPE > 0x04)
+                return null;
+
+            header.BODYLEN = BitConverter.ToInt32(hBytes, 1);
+
+            //Read Body
+            sizeToRead = header.BODYLEN;
+            byte[] bBytes = new byte[sizeToRead];
+
+            if (reader.Read(bBytes, 0, sizeToRead) == 0) //fail
+                return null;
+
+            Body body = null;
+            switch (header.MSGTYPE) {
+                case CONSTANT.CHATTING:
+                    body = new BodyChat(bBytes);
+                    break;
+                case CONSTANT.NOTICE:
+                    body = new BodyNotice(bBytes);
+                    break;
+                case CONSTANT.CHANGE_NICKNAME:
+                    body = new BodyNickname(bBytes);
+                    break;
+                case CONSTANT.FLOODING:
+                    body = new BodyFlooding(bBytes);
+                    break;
+            }
+
+            return new Message { header = header, body = body };
+        }
+    }
+}
---
+++ ChattingMessage/Header.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChattingMessage {
+    public class Header {
+        public byte MSGTYPE { get; set; }
+        public int BODYLEN { get; set; }
+
+        public Header() { }
+
+        public Header(byte MSGTYPE, int BODYLEN) {
+            this.MSGTYPE = MSGTYPE;
+            this.BODYLEN = BODYLEN;
+        }
+
+        public Header(byte[] bytes) {
+            MSGTYPE = bytes[0];
+            BODYLEN = BitConverter.ToInt32(bytes, 1);
+        }
+        
+        public byte[] GetBytes() {
+            byte[] bytes = new byte[GetSize()];
+            bytes[0] = MSGTYPE;
+
+            BitConverter.GetBytes(BODYLEN).CopyTo(bytes, 1);
+
+            return bytes;
+        }
+
+        public int GetSize() {
+            return sizeof(byte) + sizeof(int);
+        }
+    }
+}
